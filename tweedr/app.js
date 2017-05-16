b3baf70c46872bad2261b727fe2756f76350da82
vince abruzzo <vgabruzzo@gmail.com>
2017-05-16 16:35:18 -0400

    changed port

--- tweedr/app.js
+++ tweedr/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 /* setting up port & listen */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
