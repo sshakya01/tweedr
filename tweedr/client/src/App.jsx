9748668de39f045394a1181c9412735295b28d72
Jason Soukup <jrsoukup17@gmail.com>
2017-09-16 13:58:05 -0400

    adjusted header in app

--- tweedr/client/src/App.jsx
+++ tweedr/client/src/App.jsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,9 +11,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Tweedr</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
