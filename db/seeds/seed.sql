9f642ece1055f4a32314445692642b7fb669b2a0
J Tokuhisa <TokuhisaWinston@Outlook.com>
2017-10-31 17:55:23 -0400

    migrates and seeds database for heroku

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -1,6 +1,4 @@
-\c tweedr_dev
-
-INSERT INTO tweeds (tweed_text, tweed_time) VALUES 
+INSERT INTO tweeds (tweed_text, tweed_time) VALUES
   ('Hello World!', 1494788500041),
   ('I love using Tweedr... so much better than twitter.', 1494788543350),
   ('React is the best!', 1494788564011);
